a2751f1dd7be7db920891b81d8c9cd1a51732d06
sosuisen <hidekazu.kubota@gmail.com>
2024-07-23 21:35:45 +0900

    fix: use root util

--- src/main/java/com/example/controller/PublicAreaController.java
+++ src/main/java/com/example/controller/PublicAreaController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.mvc.Controller;
 import jakarta.mvc.Models;
+import jakarta.mvc.MvcContext;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
@@ -14,6 +16,13 @@ import jakarta.ws.rs.QueryParam;
 public class PublicAreaController {
 	@Inject
 	private Models models;
+	@Inject
+	private MvcContext mvcContext;
+
+	@PostConstruct
+	public void setModels() {
+		models.put("root", mvcContext.getBasePath());
+	}
 
 	@GET
 	public String home() {
